6c35416515320c544212ff440a26fe4cedafe866
hengchu <hengchu.zhang@gmail.com>
2015-04-05 16:29:45 -0400

    fixed a bunch of warnings and refactored

--- simulation.hs
+++ simulation.hs
@@ -25,7 +25,7 @@ frameSteps :: Int
 frameSteps = 6
 
 stepDelta :: H.Time
-stepDelta = framePeriod / (fromIntegral frameSteps)
+stepDelta = framePeriod / fromIntegral frameSteps
 
 worldSize :: Dimension
 worldSize = (1200, 800)
@@ -59,8 +59,8 @@ newSimST (w, h) = do space <- H.newSpace
                      H.gravity space S.$= 0 |+| -230
 
                      ground <- H.newBody H.infinity H.infinity
-                     let w2 = (fromIntegral w)/2 :: Double
-                     let groundShapeType = (H.LineSegment (-w2|+|0) (w2|+|0) 1)
+                     let w2 = fromIntegral w / 2 :: Double
+                     let groundShapeType = H.LineSegment (-w2|+|0) (w2|+|0) 1
                      groundshape <- H.newShape ground groundShapeType  $ 0|+|0
                      H.elasticity groundshape S.$= 1.0
                      H.position ground S.$= w2 |+| 0
@@ -78,7 +78,7 @@ newSimST (w, h) = do space <- H.newSpace
                                       v2 <- liftIO $ S.get $ H.velocity b2
                                       liftIO $ modifyIORef collisions ((head ps, abs $ H.len $ v2-v1):)
 
-                     H.setDefaultCollisionHandler space $
+                     H.setDefaultCollisionHandler space
                        H.Handler {H.beginHandler = Nothing
                                  ,H.preSolveHandler = Nothing
                                  ,H.postSolveHandler = Just handler
@@ -91,7 +91,7 @@ newSimST (w, h) = do space <- H.newSpace
 destroySimST :: SimST -> IO ()
 destroySimST st = H.freeSpace $ stSpace st
 
-class ObjectType a where
+class PhysicalObjDef a where
   mass :: a -> H.Mass
   moment :: a -> H.Moment
   pos :: a -> H.Position
@@ -106,7 +106,7 @@ data BallDef = BallDef {ballPos::H.Position
                        ,ballMoment::H.Moment
                        }
 
-instance ObjectType BallDef where
+instance PhysicalObjDef BallDef where
   mass = ballMass
   moment = ballMoment
   pos = ballPos
@@ -119,20 +119,20 @@ data SquareDef = SquareDef {squarePos::H.Position
                            ,squareMass::H.Mass
                            ,squareMoment::H.Moment}
 
-instance ObjectType SquareDef where
+instance PhysicalObjDef SquareDef where
   mass = squareMass
   moment = squareMoment
   pos = squarePos
   ela = squareElasticity
   shape (SquareDef {squareSideLength=sidelength}) = 
     let sidelen2 = sidelength / 2
-        vertices = [ (H.Vector (-sidelen2) (-sidelen2))
-                   , (H.Vector (-sidelen2) sidelen2)
-                   , (H.Vector sidelen2 sidelen2)
-                   , (H.Vector sidelen2 (-sidelen2))]
+        vertices = [ H.Vector (-sidelen2) (-sidelen2)
+                   , H.Vector (-sidelen2) sidelen2
+                   , H.Vector sidelen2 sidelen2
+                   , H.Vector sidelen2 (-sidelen2)]
     in  H.Polygon vertices
 
-addObj :: (ObjectType a) => a -> S.StateVar SimST -> IO ()
+addObj :: (PhysicalObjDef a) => a -> S.StateVar SimST -> IO ()
 addObj def st = do
   st' <- S.get st
   body <- H.newBody (mass def) (moment def)
@@ -153,9 +153,8 @@ addObj def st = do
   st S.$= st'{stObjects=objs'}
 
 -- Decorated function for UISF
-uisfAddObj :: (ObjectType a) => SEvent a -> S.StateVar SimST -> IO ()
-uisfAddObj (Just def) st = addObj def st
-uisfAddObj _ _ = return ()
+uisfAddObj :: (PhysicalObjDef a) => a -> S.StateVar SimST -> IO ()
+uisfAddObj = addObj
 
 extractCollision :: S.StateVar SimST -> IO [Collision]
 extractCollision st = do
@@ -173,11 +172,11 @@ removeOutOfSight st =
   do st' <- S.get st
      let (w, _) = stDimension st'
      let objmap = stObjects st'
-     newst <- foldM (f $ fromIntegral w) objmap $ M.assocs objmap
+     newst <- foldM (foldhelper $ fromIntegral w) objmap $ M.assocs objmap
      st S.$= st'{stObjects=newst}
   where
-    f width objs (o@(O b _), remove) = do
-      H.Vector x y <- S.get $ H.position b
+    foldhelper width objs (o@(O body _), remove) = do
+      H.Vector x y <- S.get $ H.position body
       if y < (-350) || abs x > (width+400)
          then remove >> return (M.delete o objs)
          else return objs
@@ -186,7 +185,7 @@ instance Simulatable SimST where
   advance st = do removeOutOfSight st
                   st' <- S.get st
                   let step = H.step (stSpace st') stepDelta
-                  replicateM_ (frameSteps) (step)
+                  replicateM_ frameSteps step
 
 
 {-
@@ -223,11 +222,11 @@ class Drawable a where
 
 instance Drawable Object where
   draw (O _ shapesAndShapeTypes)
-       v@(H.Vector x y)
+       v@(H.Vector x0 y0)
        (_, (w, h)) =
-      let (cx, cy) = convCoord (w, h) (round x, round y)
+      let (cx, cy) = convCoord (w, h) (round x0, round y0)
           bodydot = withColor Black $ ellipse (cx-2, cy-2) (cx+2, cy+2)
-          shapes = map (draw' v) $ map snd shapesAndShapeTypes
+          shapes = map (draw' v . snd) shapesAndShapeTypes
       in  foldr overGraphic nullGraphic $ bodydot:shapes
     where draw' :: H.Position -> H.ShapeType -> Graphic
           draw' (H.Vector x y) shapetype =
@@ -235,18 +234,19 @@ instance Drawable Object where
                 graphic = case shapetype of
                   H.Circle r -> let upperleft = (xo - round r, yo - round r)
                                     lowerright = (xo + round r, yo + round r)
-                                in  withColor' (rgba 100 100 100 80) $ ellipse upperleft lowerright
-                  H.LineSegment start end thickness ->
+                                in  withColor' defColor $ ellipse upperleft lowerright
+                  H.LineSegment start end _ ->
                                 let H.Vector sx sy = start
                                     H.Vector ex ey = end
-                                    s = convCoord (w, h) (round (x+sx), round (y+sy))
-                                    e = convCoord (w, h) (round (x+ex), round (y+ey))
-                                in FRP.UISF.SOE.line s e
+                                    startUISF = convCoord (w, h) (round (x+sx), round (y+sy))
+                                    endUISF = convCoord (w, h) (round (x+ex), round (y+ey))
+                                in FRP.UISF.SOE.line startUISF endUISF
                   H.Polygon vertices ->
-                                let vertices' = map (+ (H.Vector x y)) vertices
-                                    verticesUISF = map (\(H.Vector x y) -> convCoord (w, h) (round x, round y)) vertices'
-                                in  withColor' (rgba 100 100 100 80) $ polygon verticesUISF
+                                let vertices' = map (+ H.Vector x y) vertices
+                                    verticesUISF = map (\(H.Vector x1 y1) -> convCoord (w, h) (round x1, round y1)) vertices'
+                                in  withColor' defColor $ polygon verticesUISF
             in  graphic
+          defColor = rgba (100::Int) (100::Int) (100::Int) (80::Int)
 
 -- Position for each object in Hipmunk coordinates
 type WState = [(Object, H.Position)]
@@ -255,7 +255,7 @@ presentALayout :: Layout
 presentALayout = makeLayout (Fixed $ fst worldSize) (Fixed $ snd worldSize)
 
 presentACompute :: WState -> WState -> Rect -> UIEvent -> ((), WState, DirtyBit)
-presentACompute input st _ _ = ((), input, input /= st)
+presentACompute input0 st _ _ = ((), input0, input0 /= st)
 
 presentADraw :: Rect -> Bool -> WState -> Graphic
 presentADraw rect _ state =
@@ -271,41 +271,43 @@ uisfUpdateST st = do
   advance st
   st' <- S.get st
   let objs = M.keys $ stObjects st'
-  let f (O b _) = S.get $ H.position b
-  positions <- mapM f objs
+  let maphelper (O body _) = S.get $ H.position body
+  positions <- mapM maphelper objs
   return $ zip objs positions
 
 simulationA :: UISF (S.StateVar SimST) WState
 simulationA = uisfPipe uisfUpdateST
 
-addObjA :: (ObjectType a) => UISF (SEvent a, S.StateVar SimST) ()
-addObjA = uisfPipe (uncurry uisfAddObj)
+addObjA :: (PhysicalObjDef a) => UISF (SEvent (a, S.StateVar SimST)) (SEvent ())
+addObjA = uisfSinkE (uncurry uisfAddObj)
 
 parserSelect :: [(a, String)] -> a -> a
 parserSelect (r:_) _ = fst r
 parserSelect _ def = def
 
-uisfShouldAddBall :: (SEvent Bool, BallDef) -> SEvent BallDef
-uisfShouldAddBall (Just True, bd) = Just bd
-uisfShouldAddBall _ = Nothing
-
-addBallBtnA :: UISF () (SEvent BallDef)
-addBallBtnA = leftRight $ proc _ -> do
+addCommonA :: UISF () (H.Mass, H.Moment, Double, Double, H.Elasticity)
+addCommonA = leftRight $ proc _ -> do
   masstext   <- label "Mass" >>> textboxE "1.0"       -< Nothing
   momenttext <- label "M.Inertia" >>> textboxE "5.0"  -< Nothing
   xtext      <- label "x" >>> textboxE "400"          -< Nothing
   ytext      <- label "y" >>> textboxE "800"          -< Nothing
   elatext    <- label "Elasticity" >>> textboxE "0.9" -< Nothing
-  radiustext <- label "Radius" >>> textboxE "10"      -< Nothing
 
-  let mass   = flip parserSelect 1.0 $ reads masstext :: Double
-  let moment = flip parserSelect 5.0 $ reads momenttext :: Double
+  let massv   = flip parserSelect 1.0 $ reads masstext :: Double
+  let momentv = flip parserSelect 5.0 $ reads momenttext :: Double
   let x      = flip parserSelect 400 $ reads xtext :: Double
   let y      = flip parserSelect 800 $ reads ytext :: Double
-  let ela    = flip parserSelect 1.0 $ reads elatext :: Double
+  let elav    = flip parserSelect 1.0 $ reads elatext :: Double
+
+  returnA -< (massv, momentv, x, y, elav)
+
+addBallBtnA :: UISF () (SEvent BallDef)
+addBallBtnA = leftRight $ proc _ -> do
+  (massv, momentv, x, y, elav) <- addCommonA                       -< ()
+  radiustext                <- label "Radius" >>> textboxE "10" -< Nothing
   let radius = flip parserSelect 10 $ reads radiustext :: Double
 
-  let balldef = BallDef (x|+|y) radius ela mass moment
+  let balldef = BallDef (x|+|y) radius elav massv momentv
 
   click      <- button "AddBall" -< ()
   clicked    <- edge             -< click
@@ -313,21 +315,11 @@ addBallBtnA = leftRight $ proc _ -> do
 
 addSquareBtnA :: UISF () (SEvent SquareDef)
 addSquareBtnA = leftRight $ proc _ -> do
-  masstext   <- label "Mass" >>> textboxE "1.0"       -< Nothing
-  momenttext <- label "M.Inertia" >>> textboxE "5.0"  -< Nothing
-  xtext      <- label "x" >>> textboxE "400"          -< Nothing
-  ytext      <- label "y" >>> textboxE "800"          -< Nothing
-  elatext    <- label "Elasticity" >>> textboxE "0.9" -< Nothing
-  sidetext   <- label "Side" >>> textboxE "10"        -< Nothing
-  
-  let mass   = flip parserSelect 1.0 $ reads masstext :: Double
-  let moment = flip parserSelect 5.0 $ reads momenttext :: Double
-  let x      = flip parserSelect 400 $ reads xtext :: Double
-  let y      = flip parserSelect 800 $ reads ytext :: Double
-  let ela    = flip parserSelect 1.0 $ reads elatext :: Double
+  (massv, momentv, x, y, elav) <- addCommonA                     -< ()
+  sidetext                  <- label "Side" >>> textboxE "10" -< Nothing
   let side   = flip parserSelect 10 $ reads sidetext :: Double
 
-  let squaredef = SquareDef (x|+|y) side ela mass moment
+  let squaredef = SquareDef (x|+|y) side elav massv momentv
 
   click   <- button "AddSquare" -< ()
   clicked <- edge               -< click
@@ -338,24 +330,20 @@ collisionsA = uisfPipe extractCollision
 
 
 collision2midi :: Collision -> MidiMessage
-collision2midi (H.Vector x y, velocity) = ANote ((round $ interpolate 1200 x 16) `mod` 16)
-                                                ((round $ interpolate 800 y 127) `mod` 127)
-                                                ((round $ interpolate 500 velocity 127) `mod` 127)
-                                                (1.0)
+collision2midi (H.Vector x y, vel) = ANote (round (interpolate 1200 x 16) `mod` 16)
+                                           (round (interpolate 800 y 127) `mod` 127)
+                                           (round (interpolate 500 vel 127) `mod` 127)
+                                           1.0
   -- Interpolate a val bwteen [0, max] to a value in [0, range]
-  where interpolate max val range = val * (range / max)
-
-uisfPrint :: (Show a) => Maybe a -> IO ()
-uisfPrint (Just s) = print s
-uisfPrint Nothing = return ()
+  where interpolate max0 val range = val * (range / max0)
 
 present :: S.StateVar SimST -> UISF () ()
 present st = proc _ -> do
   devid              <- selectOutput                     -< ()
   squaredef          <- addSquareBtnA                    -< ()
   balldef            <- addBallBtnA                      -< ()
-  _                  <- addObjA <<< delay (Nothing, st)  -< (balldef, st)
-  _                  <- addObjA <<< delay (Nothing, st)  -< (squaredef, st)
+  _                  <- addObjA <<< delay Nothing        -< maybe Nothing (\bd -> Just (bd, st)) balldef
+  _                  <- addObjA <<< delay Nothing        -< maybe Nothing (\bd -> Just (bd, st)) squaredef
   wstate             <- simulationA                      -< st
   collisions         <- collisionsA                      -< st
   collisionsuniq     <- unique                           -< collisions
